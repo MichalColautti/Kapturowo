fed84ea85d82729666828c7fac11cdea70e5c15b
IgorFratczak <245804@edu.p.lodz.pl>
2025-04-23 13:18:22 +0200

    add vite+react

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+// backend/src/index.js
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello from Backend!" });
+});
+
+app.listen(port, () => {
+  console.log(`Backend running at http://localhost:${port}`);
+});
