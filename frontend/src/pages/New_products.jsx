baeebf11471016c8d4ba2c18b02878b8005bb9c7
MichalC <m.colautti@o2.pl>
2025-05-28 11:11:11 +0200

    Fixed new products page to take products from database

--- frontend/src/pages/New_products.jsx
+++ frontend/src/pages/New_products.jsx
@@ -1,76 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Products_grid from "../components/Products_grid";
 
-const productsData = [
-  {
-    id: 1,
-    name: "Produkt 1",
-    price: "100zł",
-    imageUrl: "/image_slider/img_2.jpg",
-  },
-  {
-    id: 2,
-    name: "Produkt 2",
-    price: "150zł",
-    imageUrl: "/image_slider/img_2.jpg",
-  },
-  {
-    id: 3,
-    name: "Produkt 3",
-    price: "200zł",
-    imageUrl: "/image_slider/img_2.jpg",
-  },
-  {
-    id: 4,
-    name: "Produkt 4",
-    price: "120zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 5,
-    name: "Produkt 5",
-    price: "180zł",
-    imageUrl: "/image_slider/img_3.jpg",
-  },
-  {
-    id: 6,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 7,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 8,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 8,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 8,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-  {
-    id: 8,
-    name: "Produkt 6",
-    price: "250zł",
-    imageUrl: "/image_slider/img_1.jpg",
-  },
-];
-
 function New_Products() {
+    const [latestProducts, setLatestProducts] = useState([]);
+  
+    useEffect(() => {
+      // Najnowsze produkty
+      fetch('/api/products/latest')
+        .then(res => res.json())
+        .then(data => setLatestProducts(data))
+        .catch(err => console.error('Błąd pobierania najnowszych produktów:', err));
+    }, []);
+
   return (
     <div className="container mt-4">
       <div className="mb-4">
@@ -81,7 +22,7 @@ function New_Products() {
           Nowości
         </h2>
       </div>
-      <Products_grid products={productsData} />
+      <Products_grid products={latestProducts} />
     </div>
   );
 }
